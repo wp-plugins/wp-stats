5099aa0c838031f2d015a2d87c06676ceaba8e1b
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-01-04 15:08:35 +0000

    Stats Options Top % Stats Fixed
    git-svn-id: https://plugins.svn.wordpress.org/wp-stats/trunk@7206 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stats/stats-options.php
+++ stats/stats-options.php
@@ -108,7 +108,7 @@ if($_POST['Submit']) {
 								echo ' />&nbsp;&nbsp;'.__('WP-UserOnline', 'wp-stats').'<br />'."\n";
 							}
 						?>
-						<p><strong><?php sprintf(_e('Top %s Stats', 'wp-stats'), get_settings('stats_mostlimit')); ?></strong></p>
+						<p><strong><?php printf(__('Top %s Stats', 'wp-stats'), get_settings('stats_mostlimit')); ?></strong></p>
 						<input type="checkbox" name="stats_display[]" value="recent_posts"<?php checked(1, $stats_display['recent_posts']); ?> />&nbsp;&nbsp;<?php echo $stats_mostlimit ?> <?php _e('Most Recent Posts', 'wp-stats'); ?><br />
 						<input type="checkbox" name="stats_display[]" value="recent_commtents"<?php checked(1, $stats_display['recent_commtents']); ?> />&nbsp;&nbsp;<?php echo $stats_mostlimit ?> <?php _e('Most Recent Comments', 'wp-stats'); ?><br />
 						<input type="checkbox" name="stats_display[]" value="commented_post"<?php checked(1, $stats_display['commented_post']); ?> />&nbsp;&nbsp;<?php echo $stats_mostlimit ?> <?php _e('Most Commented Posts', 'wp-stats'); ?><br />
